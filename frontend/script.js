870b555f56052002da24a1e95397e38130a18c06
woodmanben <woodmanben01@gmail.com>
2020-02-18 20:57:57 -0500

    New CSS

--- frontend/script.js
+++ frontend/script.js
@@ -7,11 +7,14 @@ const INPUT = document.querySelector("input");
 function addTask(e) {
     if (INPUT.value.length !== 0) {
         e.preventDefault();
-        LIST.innerHTML += `<li class="task"><a class="check" href="#"> ✖ </a><a class="entry" href="#"> ${INPUT.value} </a></li>`;
+        // Bad code, edit later:
+        LIST.innerHTML += `<li class="task"><a class="check" href="#"> ✖ </a>
+            <a class="entry" href="#"> ${INPUT.value} </a> | <a class="edit" href="#">Edit</a></li>`;
         INPUT.value = "";
-        // Add listener for the new task
+        // Adds listener for the new task
         let checks = LIST.querySelectorAll(".check");
-        let items = LIST.querySelectorAll(".entry");
+        let items = LIST.querySelectorAll(".edit");
+        // Might be possible to optimize this so that no loop is needed:
         for (i = 0; i < checks.length; i++) {
             checks[i].addEventListener('click', removeTask, false);
             items[i].addEventListener('click', toggleExpansion, false);
@@ -32,7 +35,23 @@ function compileList() {
     for (let i = 0; i < (tableSpaces.length / 2); i++) {
         let a = 2 * i + 1;
         if (i < tasks.length) {
-            tableSpaces[a].innerHTML = tasks[i].innerHTML;
+            var el = tasks[i];
+            // Clear text of any child elements (Seb's bug fix)
+            if (el.firstChild) {
+                var child = el.firstChild;
+                var texts = [];
+                while (child) {
+                    if (child.nodeType == 3) {
+                        texts.push(child.data);
+                        console.log(texts);
+                    }
+                    child = child.nextSibling;
+                }
+            }
+
+            var text = texts.join("");
+            tableSpaces[a].innerHTML = text;
+
         } else {
             tableSpaces[a].innerHTML = "";
         }
@@ -42,6 +61,7 @@ function compileList() {
 // Show task details
 function toggleExpansion(e) {
     e.preventDefault();
+    // If there is no child element for the task:
     if (this.children.length == 0) {
         var open = false;
     } else {
@@ -49,13 +69,16 @@ function toggleExpansion(e) {
     }
     if (!open) {
         open = true;
-        var para = document.createElement("p");
-        var node = document.createTextNode("Details...");
-        para.appendChild(node);
-        this.appendChild(para);
+        var div = document.createElement("div");
+        div.setAttribute("class", "details");
+        var p = document.createElement("p");
+        var text = document.createTextNode("Yee");
+        p.appendChild(text);
+        div.appendChild(p);
+        this.appendChild(div);
     } else {
         open = false;
-        this.querySelector("p").remove();
+        this.querySelector("div").remove();
     }
 }
 
